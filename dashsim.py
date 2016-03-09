b35e8782364bbe8d0fbb0243a88ec0245c76630d
Alex <ahd985@gmail.com>
2016-03-08 19:36:57 -0500

    Updated example #1

--- dashsim.py
+++ dashsim.py
@@ -31,6 +31,7 @@ class MainHandler(tornado.web.RequestHandler):
 class DashSim:
     def __init__(self):
         self.cache = {}
+        self.collector_kwargs = {}
 
     def add_chart(self, chart_id):
         self.cache[chart_id] = {}
@@ -39,14 +40,8 @@ class DashSim:
         if chart_id in self.cache:
             del self.cache[chart_id]
 
-    # Function completely replaces chart data
-    # Consider rewriting to only add what is needed.
-    def update_chart_data(self, chart_id, chart_data):
-        if chart_id in self.cache:
-            self.cache[chart_id] = chart_data
-
     def set_collector(self, collector):
-        if not isinstance():
+        if not issubclass(collector, DataCollectorMeta):
             raise TypeError('The "collector" input must inherit "DataCollectorMeta" meta class.')
 
         self.collector = collector
@@ -55,7 +50,11 @@ class DashSim:
         if not hasattr(self, 'collector'):
             raise AttributeError('Please provide a data collector class via the "set_collector" class method.')
 
-        self.cache = self.collector.collect_data(*args, **kwargs)
+        self.cache = self.collector.collect_data(self.collector_kwargs)
+
+    # Future implementation
+    def set_collector_kwargs(self, collector_kwargs):
+        pass
 
     def start_server(self, port=8888):
         self.call_collector()
@@ -74,7 +73,7 @@ class DashSim:
 
 class DataCollectorMeta(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def collect_data(self, *args, **kwargs):
+    def collect_data(self, **kwargs):
         """Abstract method that must be implemented for data collection"""
         return
 
--- examples/example_1/example_1.py
+++ examples/example_1/example_1.py
@@ -7,22 +7,27 @@ import dashsim
 # Provide chart ids - these can be found in the template (html) file
 dash = dashsim.DashSim()
 
-# Add chart data
-# Pre-generated data
-trace1 = {
-  'x': [1, 2, 3, 4],
-  'y': [10, 15, 13, 17],
-  'type': 'scatter'
-}
-trace2 = {
-  'x': [1, 2, 3, 4],
-  'y': [16, 5, 11, 9],
-  'type': 'scatter'
-}
-data = [trace1, trace2]
 
+class DataCollector(dashsim.DataCollectorMeta):
+    def collect_data(self):
+        # Add chart data
+        # Pre-generated data
+        trace1 = {
+          'x': [1, 2, 3, 4],
+          'y': [10, 15, 13, 17],
+          'type': 'scatter'
+        }
+        trace2 = {
+          'x': [1, 2, 3, 4],
+          'y': [16, 5, 11, 9],
+          'type': 'scatter'
+        }
+        data = [trace1, trace2]
+
+        return {'chart-1': data}
+
+dash.set_collector(DataCollector)
 dash.add_chart('chart-1')
-dash.update_chart_data('chart-1', data)
 
 # Run server
 dash.start_server(port=8894)
